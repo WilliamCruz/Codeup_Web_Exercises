5dc873cbf2b00e8ce90b298f93e9fb5bfc8b7a0c
William Martin <williammartincruz@gmail.com>
2014-06-23 18:30:33 -0500

    added pagination and limit

--- public/national_parks.php
+++ public/national_parks.php
@@ -10,13 +10,20 @@ function getOffset() {
     return ($page - 1) * 4;
 }
 
-
-
 $dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_test_db', 'william', 'vitzma_16');
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$National_Parks = $dbc->query('SELECT * FROM National_Parks LIMIT 10')->fetchAll(PDO::FETCH_ASSOC);
+$query = 'SELECT * FROM National_Parks LIMIT 4 OFFSET ' . getOffset();
+$National_Parks = $dbc->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+$count = $dbc->query('SELECT count(*) FROM National_Parks')->fetchColumn();
+
+$numPages = ceil($count / 4);
+
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$nextPage = $page +1;
+$prevPage = $page - 1;
 ?>
 
 <!DOCTYPE html>
@@ -30,8 +37,7 @@ $National_Parks = $dbc->query('SELECT * FROM National_Parks LIMIT 10')->fetchAll
 	</script>
 
 </head>
-	<body>
-
+<body>
 	<table border="1">
 		<th>Name</th>
 		<th>Location</th>
@@ -45,13 +51,20 @@ $National_Parks = $dbc->query('SELECT * FROM National_Parks LIMIT 10')->fetchAll
 			<td><?= $national_park['date_established'];?></td>
 			<td><?= $national_park['area'];?></td>
 		</tr>
-			
 	<? endforeach ?>
-
-
 	</table>
+	<?php if ($page > 1): ?>
+		<a href="?page=<?= $prevPage; ?>">Previous</a>
+	<?php endif; ?>
 
+	<?php if ($page < $numPages): ?>
+		<a href="?page=<?= $nextPage; ?>">Next</a>
+	<?php endif; ?>
 
-	</body>
+</body>
 <html>
+
+
+
+
 			
