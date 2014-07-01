6fd79468959d2ee7b21b9907072cc753616b68df
William Martin <williammartincruz@gmail.com>
2014-07-01 08:58:56 -0500

    adjusted head tag

--- public/form.php
+++ public/form.php
@@ -1,20 +1,21 @@
 <!DOCTYPE>
 <html>
-<head>Form Lesson</head>
-<title>My First HTML Form</title>
+<head>
+    <title>My First HTML Form</title>
+<head>
 <body>
-	<form method="POST">
-		<p>
-			<label for="Username">Username</label>
-			<input id="username" name="username" type="text">
-		</p>
-		<p>
-			<label for="password">Password</label>
-			<input id="password" name="password" type="text">
-		</p>
-		<p>
-			<input type="submit">
-		</p>
-	</form>
+    <form method="POST">
+        <p>
+            <label for="Username">Username</label>
+            <input id="username" name="username" type="text">
+        </p>
+        <p>
+            <label for="password">Password</label>
+            <input id="password" name="password" type="text">
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+    </form>
 </body>
 </html>
