e28ed8a00e7d7cf477e1c4af25ce11233f11bf42
William Martin <williammartincruz@gmail.com>
2014-07-01 17:58:13 -0500

    removed var_dump and removed space from bottom

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -1,110 +1,68 @@
-<?php
-
-var_dump($_GET);
-var_dump($_POST)
-
-?>
+<!DOCTYPE>
 <html>
-	<head>
-		<title>My First HTML Form</title>
-	</head>
+    <head>
+        <title>My First HTML Form</title>
+    </head>
 <body>
-	<h2>User Login</h2>
-	<form method="POST" action="http://requestb.in/skj98bsk">
-		<p>
-			<label for="Username">Username</label>
-			<input id="username" name="username" type="text" placeholder="Username">
-		</p>
-		<p>
-			<label for="password">Password</label>
-			<input id="password" name="password" type="text" placeholder="Password">
-		</p>
-		<p>
-			<button type="submit">Login</button>
-		</p>
-	</form>
-	<h2>Compose Email</h2>
-	<form method="POST" action="http://requestb.in/skj98bsk">
-		<p> 
-			<label for="text">Text</label>
-			<input id="text" name="text" type="text" placeholder="Text">
-		</p>
-		<p>
-			<label for="to">To  </label>
-		 	<input id="to" name="to" type="text" placeholder="To">
-		</p>
-		<p>
-			<label for="from">From</label>
-		 	<input id="from" name="from" type="text" placeholder="From">
-		</p>
-		<p>
-			<label for="subject">Subject</label>
-		 	<input id="subject" name="subject" type="text" placeholder="Subject">
-		</p>
-		<p>
-			<label for="body">eMail Body</label>
-			<textarea id="email_body" name="email_body"></textarea>
-			<button type="submit">Submit</button>
-		</p>
-	</form>
-	<h2>Select Testing</h2>
-	<form method="POST" action="http://requestb.in/skj98bsk">
-		<p>Would you recommend Codeup to anyone?</p>
-			<label for="answer">Answer</label>
-			<select id="answer" name="answer">
-				<option value="1">Yes</option>
-				<option value="0"selected>No</option>
-			</select>
-			<button type="submit">Submit</button>
+    <h2>User Login</h2>
+    <form method="POST" action="http://requestb.in/skj98bsk">
+        <p>
+            <label for="Username">Username</label>
+            <input id="username" name="username" type="text" placeholder="Username">
+        </p>
+        <p>
+            <label for="password">Password</label>
+            <input id="password" name="password" type="text" placeholder="Password">
+        </p>
+        <p>
+            <button type="submit">Login</button>
+        </p>
+    </form>
+    <h2>Compose Email</h2>
+    <form method="POST" action="http://requestb.in/skj98bsk">
+        <p> 
+            <label for="text">Text</label>
+            <input id="text" name="text" type="text" placeholder="Text">
+        </p>
+        <p>
+            <label for="to">To  </label>
+            <input id="to" name="to" type="text" placeholder="To">
+        </p>
+        <p>
+            <label for="from">From</label>
+            <input id="from" name="from" type="text" placeholder="From">
+        </p>
+        <p>
+            <label for="subject">Subject</label>
+            <input id="subject" name="subject" type="text" placeholder="Subject">
+        </p>
+        <p>
+            <label for="body">eMail Body</label>
+            <textarea id="email_body" name="email_body"></textarea>
+            <button type="submit">Submit</button>
+        </p>
+    </form>
+    <h2>Select Testing</h2>
+    <form method="POST" action="http://requestb.in/skj98bsk">
+        <p>Would you recommend Codeup to anyone?</p>
+            <label for="answer">Answer</label>
+            <select id="answer" name="answer">
+                <option value="1">Yes</option>
+                <option value="0"selected>No</option>
+            </select>
+            <button type="submit">Submit</button>
 
-	<h2>Multiple Choice</h2>
-		<p>What languages are tought at Codeup?</P>
-			<select>
-				<option value="Linux">Linux</option>
-				<option value="Apache">Apache</option>
-				<option value="MYSQL">MYSQL</option>
-				<option value="PHP">PHP</option>
-				<option value="Javascript">Javascript</option>
-				<option value="All of the Above">All of the Above</option>
-			</select>
-			<button type="submit">Submit</button>
-	</form>
+    <h2>Multiple Choice</h2>
+        <p>What languages are tought at Codeup?</P>
+            <select>
+                <option value="Linux">Linux</option>
+                <option value="Apache">Apache</option>
+                <option value="MYSQL">MYSQL</option>
+                <option value="PHP">PHP</option>
+                <option value="Javascript">Javascript</option>
+                <option value="All of the Above">All of the Above</option>
+            </select>
+            <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
-
-
-
-
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
