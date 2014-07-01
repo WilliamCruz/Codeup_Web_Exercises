818d31c24760bbf932d80f8e1e736d6e463a97c8
William Martin <williammartincruz@gmail.com>
2014-07-01 08:58:30 -0500

    adjusted spacing

--- public/form2.php
+++ public/form2.php
@@ -1,27 +1,21 @@
-<?php
-
-var_dump($_GET);
-var_dump($_POST)
-
-?>
+<!DOCTYPE>
 <html>
 <head>
 	<title>My First HTML Form</title>
 </head>
 <body>
-		<form method="POST">
-
-			<p>
-				<label for="Username">Username</label>
-				<input id="username" name="username" type="text">
-			</p>
-			<p>
-				<label for="password">Password</label>
-				<input id="password" name="password" type="text">
-			</p>
-			<p>
-				<input type="submit">
-			</p>
-		</form>
+	<form method="POST">
+		<p>
+			<label for="Username">Username</label>
+			<input id="username" name="username" type="text">
+		</p>
+		<p>
+			<label for="password">Password</label>
+			<input id="password" name="password" type="text">
+		</p>
+		<p>
+			<input type="submit">
+		</p>
+	</form>
 </body>
 </html>
